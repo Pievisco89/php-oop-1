eb739353abfc343842683183ff5f1d0245bca556
Pievisco89 <pie-89@live.it>
2021-06-17 16:30:18 +0200

    Esercizio Finito

--- Movie.php
+++ Movie.php
@@ -5,12 +5,19 @@
     public $title;
     public $director;
     public $year;
+    private $recommended_age;
+    private $recommended_msg;
+
+    static $counter = 0;
 
     /* Costruttore */
-    public function __construct($_title)
+    public function __construct($_title,  $_recommended_age)
     {
 
       $this->title = $_title;
+      $this->recommended_age = $_recommended_age;
+      $this->setRecommended_msg();
+      self::$counter++;
 
     }
 
@@ -18,6 +25,27 @@
     public function setYear($_year)
     {
       $this->year = $_year;
+    }
+
+    public function getRecommended_msg()
+    {
+      return $this->recommended_msg;
+    }
+
+    /* Metodo privato */
+    private function setRecommended_msg()
+    {
+      if($this->recommended_age < 14){
+        $this->recommended_msg = "Visione adatta a tutti!";
+      }else{
+        $this->recommended_msg = "Vietato ai minori di 14 anni!";
+      }
+    }
+
+    /* Metodo statico */
+    static function getCounter()
+    {
+      return self::$counter;
     }
 
 
--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 
   require_once __DIR__ . '/Movie.php';
 
-  $tenenbaum = new Movie('I Tenenbaum');
+  $tenenbaum = new Movie('I Tenenbaum', 1);
   $tenenbaum->director = 'Wes Anderson';
   $tenenbaum->year = '2001';
 
-  $fight_club = new Movie('Fight Club');
+  $fight_club = new Movie('Fight Club', 14);
   $fight_club->director = 'David Fincher';
   $fight_club->setYear('1999');
 
@@ -27,12 +27,17 @@
   <h2>Film: <?php echo $tenenbaum->title ?></h2>
   <h2>Regista: <?php echo $tenenbaum->director ?></h2>
   <h2>Anno: <?php echo $tenenbaum->year ?></h2>
+  <span><?php echo $tenenbaum->getRecommended_msg() ?></span>
 
   <hr>
 
   <h2>Film: <?php echo $fight_club->title ?></h2>
   <h2>Regista: <?php echo $fight_club->director ?></h2>
   <h2>Anno: <?php echo $fight_club->year ?></h2>
+  <span><?php echo $fight_club->getRecommended_msg() ?></span>
+
+
+  <h4>Film inseriti: <?php echo Movie::getCounter() ?>. </h4>
   
 </body>
 </html>
